3151cfffc2dbd0d9264d23d9ed3074adae39f0e6
boma25 <amakirisamuelboma@gmail.com>
2020-11-25 01:15:41 +0100

    Fix:bugfix

--- src/util/store.js
+++ src/util/store.js
@@ -2,7 +2,7 @@ import React,{createContext, useState, useContext} from "react"
 
 const StoreContext = createContext(null)
 const StoreProvider = ({children})=>{
-    const [isLoggedIn, setIsLoggedIn]= useState(true)
+    const [isLoggedIn, setIsLoggedIn]= useState(false)
     const [name, setName] = useState("")
     return (
         <StoreContext.Provider value={{isLoggedIn, setIsLoggedIn, name, setName}}>
